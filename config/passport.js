6a9d14a9df1e3bbd29237c2e9abe77cf377f6414
Demilululu <demilululu@gmail.com>
2021-08-15 00:18:19 +0800

    Add facebook login

--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ const passport = require('passport')
 const bcrypt = require('bcryptjs')
 
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 
 const User = require('../models/user')
 
@@ -29,6 +30,33 @@ module.exports = (app) => {
       })
       .catch(error => done(error, false))
   }))
+
+  //Set Facebook Strategy
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  },
+    function (accessToken, refreshToken, profile, done) {
+      const { name, email } = profile._json
+      User.findOne({ email })
+        .then(user => {
+          if (user) {
+            return done(null, user)
+          }
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash => {
+              User.create({ name, email, password: hash })
+            })
+            .then(user => done(null, user))
+            .catch(error => done(error, false))
+        })
+    }
+  ));
 
   //Serialise and Deserialise
   passport.serializeUser((user, done) => {
