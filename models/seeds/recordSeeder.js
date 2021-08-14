f74a98b9ad29f0c4b2d7003e8e2ef53eb9bb8bf1
Demilululu <demilululu@gmail.com>
2021-08-14 23:32:32 +0800

    Modify recordSeeder and modify login/register css

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,11 +1,31 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const Record = require('../record')
-const recordData = require('./record.json')
-const recordList = recordData.results
+const User = require('../user')
+const recordList = require('./record.json').results
 
 const db = require('../../config/mongoose')
 
+const SEED_USER = {
+  name: 'User1',
+  email: 'user1@example.com',
+  password: '12345678'
+}
+
 db.once('open', () => {
-  Record.create(...recordList)
+
+  return bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({ name: SEED_USER.name, email: SEED_USER.email, password: hash }))
+    .then(user => {
+      const userId = user._id
+      recordList.forEach(record => record.userId = userId)
+      return Record.create(...recordList)
+    })
     .then(() => {
       console.log('record seeder done!')
       db.close()
